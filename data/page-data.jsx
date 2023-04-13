e956ddc3c25a980c22b510b010f9a78714bafc22
syed kashif raza rizvi <kashifr684@gmail.com>
2023-04-13 19:46:20 +0500

    feat: update json

--- data/page-data.jsx
+++ data/page-data.jsx
@@ -1,68 +1,67 @@
 export const personalData = {
-	name: "Kashif Raza",
-	role: "Electrical Engineer",
-	education: [
-		"Bachelor of Technology in Electrical Engineering, NEDUET(2013-2016)",
-		"Master of Science in Electrical Engineering, MAJU (2018-2021)",
-	],
-	contactLinks: [
-		"mailto:kashifr684@gmail.com",
-		"https://twitter.com/Razasahab_85",
-		"https://linkedin.com/aliciarodriguezLi",
-		"https://medium.com/@time2code.ali",
-	],
+  name: "Kashif Raza",
+  role: "Electrical Engineer",
+  education: [
+    "Bachelor of Technology in Electrical Engineering, NEDUET(2013-2016)",
+    "Master of Science in Electrical Engineering, MAJU (2018-2021)",
+  ],
+  contactLinks: [
+    "mailto:kashifr684@gmail.com",
+    "https://twitter.com/Razasahab_85",
+    "https://www.linkedin.com/in/kashif-raza-rizvi-823391157/",
+    "https://medium.com/@time2code.ali",
+  ],
 };
 
 export const aboutMe = {
-	title: "About Me",
-	body: [
-		"As a highly motivated and driven individual with a passion for making a positive impact, I have a unique blend of technical and interpersonal skills, thanks to my background in computer science. I am able to tackle complex problems with ease, and I never lose sight of my ultimate goal, even when facing challenges.",
-		"When I'm not working, I love hiking, reading, and spending time with my friends and family. I believe that my determination, can-do attitude, and hard work make me a true asset to any team.",
-	],
+  title: "About Me",
+  body: [
+    "As a highly motivated and driven individual with a passion for making a positive impact, I have a unique blend of technical and interpersonal skills, thanks to my background in electrical. I am able to tackle complex problems with ease, and I never lose sight of my ultimate goal, even when facing challenges.",
+    "When I'm not working, I love hiking, reading, and spending time with my friends and family. I believe that my determination, can-do attitude, and hard work make me a true asset to any team.",
+  ],
 };
 
 export const skills = {
-	soft: [
-		{ icon: "👂🏼", text: "Active Listening" },
-		{ icon: "💬", text: "Effective Communication" },
-		{ icon: "👥", text: "Collaboration" },
-		{ icon: "⽓", text: "Teamwork" },
-		{ icon: "💡", text: "Creative Problem Solving" },
-		{ icon: "⌛️", text: "Time management" },
-	],
-	hard: [
-		{ icon: "💻", text: "Python" },
-		{ icon: "ʦ", text: "TypeScript" },
-		{ icon: "🚀", text: "React" },
-		{ icon: "💾", text: "SQL" },
-		{ icon: "💾", text: "noSQL" },
-		{ icon: "📈", text: "Data Structures and Algorithms" },
-	],
+  soft: [
+    { icon: "👂🏼", text: "Active Listening" },
+    { icon: "💬", text: "Effective Communication" },
+    { icon: "👥", text: "Collaboration" },
+    { icon: "⽓", text: "Teamwork" },
+    { icon: "💡", text: "Creative Problem Solving" },
+    { icon: "⌛️", text: "Time management" },
+  ],
+  hard: [
+    { text: "HT/LT Network Project Execuation" },
+    { text: "HT/LT Network Project Post Governance" },
+    { text: "HT/LT Network RCA Work" },
+    { text: "learning Blockchain" },
+	{ text: "Learning Web Developement" },
+	{ text: "Typescript" },
+	{ text: "Tailwind CSS" },
+  ],
 };
 
 export const professionalData = {
-	title: "Professional Experience",
-	experiences: [
-		{
-			role: "Data Scientist, Acme Inc. (2021-Present)",
-			description:
-				"Design and implement machine learning models to analyze large datasets and drive business decisions. Collaborate with cross-functional teams to develop and launch new products and features. Provide technical guidance and mentorship to junior data scientists. Conduct regular presentations to stakeholders on the findings and insights generated from data analysis.",
-			current: true,
-		},
+  title: "Professional Experience",
+  experiences: [
+    {
+      role: "Assistant Manager (Technical), K-ELECTRIC (2015-Present)",
+      description:
+        "•	Ensur/Hand all the operations and corrective/preventive maintenance for HT/LT (11KV/440) Distribution network. •	Initiated and successfully completed the execution and post governess guideline for LT ABC bundle cable for K-ELECTRIC Distribution Network. •	Carring out Distribution network analysis for the reduction of technical faults and to ensure the maintain TAT/KPIs and SAIFI/SAIDI. •	 Preparing corrective and preventive maintenance plans for 11KV/440 distribution networks. •	Reviewing and Updating Electrical Equipment’s specification in order to keep them in line with the latest advancements and practices in the distribution network. •	 Coordinating with several departments, contractors and consultants on their technical queries and providing the required technical data to the concerned departments. •	hands on experience of handling 11KV substations (VCB/OCB), operating 11KV RMUs (Ring main unit). •	Prior knowledge of On Load Devices and Off Load Devices like RMU, VCB, LBS (load break switch), knife switch etc. •	Winner of KE CARES CHAMPION award for maintained KPIs.",
+      current: true,
+    },
 
-		{
-			role: "Machine Learning Engineer, XYZ Corp. (2019-2021)",
-			description:
-				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
-			current: false,
-		},
-		{
-			role: "Machine Learning Engineer, ABX Corp. (2020-2021)",
-			description:
-				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
-			current: false,
-		},
-	],
+    {
+      role: "Senior Operator/ Assistant Manager (Technical), Caracrete Pvt Ltd (2006-2015)",
+      description:
+        "•	Managing Installation commissioning of concrete block plant. •	Operation of concrete plant. •	Managing production. •	Managing load cells and counting base batching station. •	Plant panel wiring (DC-Electromagnetic Hydraulic Valves). •	 AC-Contactors for three phase motors. •	Managing RAW material for production ",
+      current: false,
+    },
+    {
+      role: "Learning Blockchain & Web Developement, PIAIC. (2022-Present)",
+      description:
+        "•	Learnt basics of Blockchain. •	Learning programming languages.•	Learning Web Development both front end and backend Developement.",
+      current: false,
+    },
+  ],
 };
-
-
